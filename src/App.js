eb4957eb215d75a4338c9cfec5406635833fed15
KB <karthikbalaji.dev@gmail.com>
2023-09-03 00:03:02 +0530

    Home page correction

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routing } from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Karthik Balaji - Full Stack Developer
-        </p>
-      </header>
-    </div>
+    <Routing />
   );
 }
 
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+
+
+import React from 'react'
+import "./style.scss"
+
+const Home = () => {
+    return (
+        <div className='home'>Karthik Balaji - FullStack Developers</div>
+    )
+}
+
+export default Home
