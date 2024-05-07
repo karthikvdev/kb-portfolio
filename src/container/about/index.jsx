5475a625acdb90cc44a116cc4269303b87ed5a0b
Karthik Balaji <143891091+karthikvdev@users.noreply.github.com>
2024-05-07 13:56:04 +0530

    Update index.jsx

--- src/container/about/index.jsx
+++ src/container/about/index.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import "./style.scss"
+import React from "react";
+import "./style.scss";
 
 const About = () => {
-    return (
-        <section id="about-me">
-            <div className='title-container'>
-                <h6 className='title'>About Me</h6>
-                <p className='description'>The Generator App is an online tool that helps you to export ready-made templates ready to work as your future website. It helps you to combine slides, panels and other components and export it as a set of static files: HTML/CSS/JS.</p>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section id="about-me">
+      <div className="title-container">
+        <h6 className="title">About Me</h6>
+        <p className="description">
+          I am a well-trained, skilled, and dedicated full stack developer with
+          three years of experience developing web applications. As an
+          imaginative developer, designing web apps constantly sparks inner
+          creativity at every stage of their development. I enjoy taking on
+          challenges and creating web programmes that are both user-friendly and
+          effective.
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
